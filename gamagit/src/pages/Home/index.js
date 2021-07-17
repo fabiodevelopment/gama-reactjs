328fe3ec6f343b4125f8d1768691794c56e7f933
Fabio Haddad <fabiohpaes@gmail.com>
2021-07-17 13:20:28 -0300

    [reactjs] Adjustments msg Error from catch err

--- gamagit/src/pages/Home/index.js
+++ gamagit/src/pages/Home/index.js
@@ -6,9 +6,12 @@ import { useHistory } from 'react-router-dom';
 function Home(props) {
 	const history = useHistory();
 	const [usuario, setUsuario] = useState('');
+	const [erro, setErro] = useState(false);
+	
 	function handlePesquisa() {
 		console.log(usuario);
-		axios.get(`https://api.github.com/users/${usuario}/repos`).then(response => {
+		axios.get(`https://api.github.com/users/${usuario}/repos`)
+		.then(response => {
 			const repositories = response.data;
 			const repositoriesName = [];
 			repositories.map((repository) => {
@@ -16,15 +19,20 @@ function Home(props) {
 			});
 			localStorage.setItem('repositoriesName', JSON.stringify(repositoriesName));
 			history.push('./repositories');
+		})
+		.catch(err => {
+			setErro(true);
 		});
 	}
 	return (
-		<>
-		<S.Container>
-			<S.Input className="usuarioInput" placeholder="Usuário" value={usuario} onChange={e => setUsuario(e.target.value)} />
-			<S.Button type="button" onClick={handlePesquisa}>Pesquisar</S.Button>
-		</S.Container>
-		</>
+		<S.HomeContainer>
+			<S.Content>
+				<S.Input className="usuarioInput" placeholder="Usuário" value={usuario} onChange={e => setUsuario(e.target.value)} />
+				<S.Button type="button" onClick={handlePesquisa}>Pesquisar</S.Button>
+			</S.Content>
+			{ erro ? <S.ErrorMsg>Ocorreu um erro. Tente novamente.</S.ErrorMsg> : ''}
+				
+		</S.HomeContainer>
 	);
 }
 
--- gamagit/src/pages/Home/styled.js
+++ gamagit/src/pages/Home/styled.js
@@ -1,12 +1,20 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
-	width: 100vw;
+export const HomeContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
 	height: 100vh;
+`;
+
+export const Content = styled.div`
+	width: 100vw;
 	display: flex;
 	justify-content: center;
 	align-items: center;
 `;
+
 
 export const Input = styled.input`
 	border: 1px solid #ddd;
@@ -22,7 +30,7 @@ export const Input = styled.input`
 `;
 
 export const Button = styled.button`
-	height: 1.5rem;
+	height: 1.6rem;
 	border: 1px solid #000;
 	background: #000;
 	color: #FFF;
@@ -33,4 +41,13 @@ export const Button = styled.button`
 		outline: none;
 		box-shadow: none;
 	}
+`;
+
+export const ErrorMsg = styled.span`
+	color: red;
+	font-size: 0.7rem;
+	display:block;
+	font-weight: 600;
+	margin-top: 1rem;
+	font-family: Sans-Serif;
 `;
